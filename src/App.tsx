261c5be208bfeaa18de68e9dff707d1c062cc31b
OlgaKapskaya <olikbuko@gmail.com>
2022-10-04 19:21:05 +0300

    add .map()

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import './App.css';
 import {Todolist} from "./components/Todolist";
 
+export type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 function App() {
+
+    const tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ];
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+
     return (
         <div className="App">
-            <Todolist/>
-            <Todolist/>
+            <Todolist title={'ver 1'} tasks={tasks}/>
+            <Todolist title={'ver 2'} tasks={tasks2}/>
         </div>
     );
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,17 +1,30 @@
 import React from "react";
+import {TasksType} from "../App";
 
-export const Todolist = () => {
+type TodolistProps = {
+    title: string
+    tasks: TasksType[]
+}
+
+export const Todolist: React.FC<TodolistProps> = (props) => {
+    const {title, tasks} = props;
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                {tasks.map(elem => {
+                    return (
+                        <li key={elem.id}>
+                            <input type="checkbox" checked={elem.isDone}/>
+                            <span>{elem.title}</span>
+                        </li>
+                    )
+                })}
+
             </ul>
             <div>
                 <button>All</button>
