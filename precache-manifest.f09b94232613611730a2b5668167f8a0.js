2e01b5a35b00454174f1120054fcf094e35bff49
OlgaKapskaya <olikbuko@gmail.com>
2022-10-16 16:39:18 +0300

    Updates

---
+++ precache-manifest.f09b94232613611730a2b5668167f8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36564db3c7a20be44a271fbd6f40ba01",
+    "url": "/todolist-myversion/index.html"
+  },
+  {
+    "revision": "b8db98e0f30ddbfb9b53",
+    "url": "/todolist-myversion/static/css/main.a2955186.chunk.css"
+  },
+  {
+    "revision": "c2c7b469fbcab6d79403",
+    "url": "/todolist-myversion/static/js/2.4e8d8e4c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist-myversion/static/js/2.4e8d8e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8db98e0f30ddbfb9b53",
+    "url": "/todolist-myversion/static/js/main.23cd4b71.chunk.js"
+  },
+  {
+    "revision": "79b4fe34dc3f1888fc3e",
+    "url": "/todolist-myversion/static/js/runtime-main.047fee8b.js"
+  },
+  {
+    "revision": "0a97d6f9637f67dae3a43e425c39c8d3",
+    "url": "/todolist-myversion/static/media/trash.0a97d6f9.svg"
+  }
+]);
